2b60b85534fc70ef8e580f4f8895e1613c7c6bf1
Joe-Yang-ICD2O1 <joe.yang@student.tdsb.on.ca>
2024-10-09 13:35:33 +0000

    answer

--- PP2_1.py
+++ PP2_1.py
@@ -3,14 +3,25 @@
 def q1(): 
   #Write Assignment code here
 
+num= int(input("Input an integer: "))
+
+if num % 2 == 0:
+  print("num is even") 
+if num % 2 != 0:
+  print("num is odd")
 
 def q2(): 
   #Write Assignment code here
 
-
+name= input("Input a name:")
+if name == "Kalisz":
+  print("Teacher")
+if name != "Kalisz":
+  print("student")
 
 #Do not alter the following code
 #Comment out the following code when running your tests
-
+'''
 q1()
 q2()
+'''
